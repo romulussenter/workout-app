3e4ffdf13bcf091f445aff37a48f80501ed134d2
Romulus <romulus.senter@yahoo.com>
2018-03-20 19:44:58 -0400

    Got the button to show up on the dev tools and shows up in the console.log

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+      this.state ={
+        
+      };
+      this.addWorkout = this.addWorkout.bind(this);
+    }
+
+    addWorkout(e){
+      e.preventDefault()
+      console.log('YAY I can see you');
+      
+    }
   render() {
     return (
+    <form onSubmit={this.addWorkout}>  
       <div>
-        <h1>Hello World</h1>
+        <h1>My work out app</h1>
+        <div>
+          <input type="text" onChange={this.addWorkout} />
+          <div>
+            <button type="submit" >+</button>
+          </div>
+        </div>
       </div>
+     </form> 
     );
   }
 }
